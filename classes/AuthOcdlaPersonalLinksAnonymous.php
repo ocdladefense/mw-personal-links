c7385d2d808aae7148d108dc735a36385a3b3307
trevoruehlinx1 <trevoruehlinx1@gmail.com>
2021-12-24 10:39:32 -0800

    Clean up

--- classes/AuthOcdlaPersonalLinksAnonymous.php
+++ classes/AuthOcdlaPersonalLinksAnonymous.php
@@ -3,10 +3,12 @@
 class AuthOcdlaPersonalLinksAnonymous extends UserPreferencesPersonalLinks
 {
 	
-	public function __construct(AuthOcdlaPersonalLinksManager $manager)
-	{
+	public function __construct(AuthOcdlaPersonalLinksManager $manager) {
+
 		parent::__construct($manager);
+
 		$this->data = $this->getLinks('pt-login');
+		
 		$this->addAttribute('logged_in',false);
 	}
 
--- classes/AuthOcdlaPersonalLinksManager.php
+++ classes/AuthOcdlaPersonalLinksManager.php
@@ -1,11 +1,8 @@
 <?php
 
 
+class AuthOcdlaPersonalLinksManager {
 
-
-
-class AuthOcdlaPersonalLinksManager
-{
 	// const LOD_SKIN = '/var/www/lodtest/skins/lod/templates';
 	/**
 	 * var personalLinks
@@ -76,28 +73,23 @@ class AuthOcdlaPersonalLinksManager
 	private $useSalesforce = false;
 	
 	
-	public function __construct($settings=array())
-	{
-		global $cookiePrefix, $wgSiteDirectory,
-		  $wgAuthOcdla_LoginURL, 
-		  
-		  $wgAuthOcdla_LogoutURL;
+	public function __construct($settings = array()) {
+
+		global $cookiePrefix, $wgSiteDirectory, $wgAuthOcdla_LoginURL, $wgAuthOcdla_LogoutURL;
 		  
 		$this->loginUrl = $wgAuthOcdla_LoginURL;
 		$this->logoutUrl = $wgAuthOcdla_LogoutURL;
-		
 		$this->requestUrl = $_GET['requestUrl'];
-		$useSalesForce = false;
+
 		$this->setUserLoadMethod($settings['userLoadMethod']);
 		$this->mwUser->load();
 		$this->parseMwUserGroups();
 		$this->loadTemplates();
 	}
 	
-	private function setUserLoadMethod($setting=null)
-	{
-		switch($setting)
-		{
+	private function setUserLoadMethod($setting = null) {
+
+		switch($setting) {
 			case 'loadFromUserId':
 				$this->mwUser = User::newFromId($_COOKIE[$cookiePrefix.'UserID']);
 				break;
@@ -110,85 +102,68 @@ class AuthOcdlaPersonalLinksManager
 		}
 	}
 	
-	public function getLoginUrl()
-	{
+	public function getLoginUrl() {
+
 		global $wgAuthOcdla_HostRedirect;
-		return $this->loginUrl . '?retURL='.urlencode($wgAuthOcdla_HostRedirect .$this->requestUrl)
-			;//.'&server='.urlencode();
+
+		return $this->loginUrl . '?retURL=' . urlencode($wgAuthOcdla_HostRedirect . $this->requestUrl);
 	}
 	
-	public function isLegComm()
-	{
+	public function isLegComm() {
+
 		return $this->legComm;
 	}
 	
-	public function isSysOp()
-	{
+	public function isSysOp() {
+
 		return $this->sysOp;
 	}
 	
-	public function getLogoutUrl()
-	{
+	public function getLogoutUrl() {
+
 		global $wgAuthOcdla_HostRedirect;
-		return $this->logoutUrl . 
-			'?retURL='.urlencode($wgAuthOcdla_HostRedirect .$this->requestUrl);
+
+		return $this->logoutUrl . '?retURL='.urlencode($wgAuthOcdla_HostRedirect .$this->requestUrl);
 	}
 
-	public function listUserGroups()
-	{
-    return $this->user->mGroups;
-    /**
-     * Should output something similar to:
-     * => Array
-        (
-            [0] => bureaucrat
-            [1] => ignoreanalytics
-            [2] => legcomm
-            [3] => pageeditors
-            [4] => sysop
-        )
-      *
-      */
+	public function listUserGroups() {
+
+    	return $this->user->mGroups;
 	}
 	
-	private function loadTemplates()
-	{
-		if ($this->mwUser->mId !== 0)
-		{
-			$this->personalLinks = new AuthOcdlaPersonalLinksAuthenticated($this);
-		}
-		else
-		{
-			$this->personalLinks = new AuthOcdlaPersonalLinksAnonymous($this);
-		}
+	private function loadTemplates() {
+
+		if ($this->mwUser->mId !== 0) $this->personalLinks = new AuthOcdlaPersonalLinksAuthenticated($this);
+
+		else $this->personalLinks = new AuthOcdlaPersonalLinksAnonymous($this);
 	}
 	
 	
-	public function parseMwUserGroups()
-	{
-		$this->sysOp 			= in_array('sysop', $this->mwUser->mGroups) ? true : false;
-		$this->legComm 		= in_array('legcomm', $this->mwUser->mGroups) ? true : false;
-		$this->mName 			= $this->mwUser->mName;
-		$this->mId 				= $this->mwUser->getId();
+	public function parseMwUserGroups() {
+
+		$this->sysOp = in_array('sysop', $this->mwUser->mGroups) ? true : false;
+		$this->legComm = in_array('legcomm', $this->mwUser->mGroups) ? true : false;
+		$this->mName = $this->mwUser->mName;
+		$this->mId = $this->mwUser->getId();
 	}
 	
-	public function getLinks()
-	{
+	public function getLinks() {
+
 		return array(
 			'logged_in' => $this->personalLinks->getAttribute('logged_in'),
 			'markup' => $this->personalLinks->getAsHtml(),
 		);
 	}
 	
-	public function toJson()
-	{
-		if($this->useSalesforce)
-		$this->personalLinks->addLink('salesforce','href');
+	public function toJson() {
+
+		if($this->useSalesforce) $this->personalLinks->addLink('salesforce','href');
+
 		return json_encode($this->getLinks());
 	}
 
-	public function __toString()
-	{
+	public function __toString() {
+
 		return $this->personalLinks;
 	}
 }
